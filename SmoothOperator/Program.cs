0073a11bfe92b8bf8139658b3101bb7bbb63e928
RicardoMota22 <ricardomota.artist@gmail.com>
2025-02-26 21:39:34 +0000

    Exercício 1 Operadores ++ e --

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Console.WriteLine("Insere número inteiro: ");
+            
+        
+            int i = int.Parse(Console.ReadLine()); 
+            
+
+            
+
+        
+            sbyte n = (sbyte)i;
+        
+            Console.WriteLine((--n));
+            Console.WriteLine((++n));
+        }
+    }
+}
