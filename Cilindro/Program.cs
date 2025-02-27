4c151d5a19290e519312473d8cebb41a0b60c95c
RicardoMota22 <ricardomota.artist@gmail.com>
2025-02-27 13:37:18 +0000

    Exercício 5, 3dp

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,21 +7,28 @@ namespace Cilindro
         private static void Main(string[] args)
         {
             Console.WriteLine("Insere altura: ");
+            int aa = int.Parse(Console.ReadLine());
+
+            double a = (double)aa; 
             
         
-            double a = double.Parse(Console.ReadLine());
+            //double a = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Insere raio: ");
+            int rr = int.Parse(Console.ReadLine());
 
-            double r = double.Parse(Console.ReadLine());
-            
-        
-            
+            //double r = double.Parse(Console.ReadLine());
 
+            double r = (double)rr; 
+            double V = Math.PI * Math.Pow(r, 2) * a; //Volume using Math.Pi 
+            //and Math.Power
+            double Vo = Math.Round(V, 3); //Rounds to 3 decimal places
+            Console.WriteLine(Vo);
 
-            
-            double V = Math.PI * (r * r) * a; //Volume
-            Console.WriteLine(V);
+            double Sa = 2 * Math.PI * r * (r + a); //Surface area 
+            double Sao = Math.Round(Sa, 3); //Rounds to 3 decimal places
+
+            Console.WriteLine(Sao);
         
         }
     }
