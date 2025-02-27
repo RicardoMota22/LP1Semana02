51787a124b3c7b23d36f1e4130bafd8265fc7425
RicardoMota22 <ricardomota.artist@gmail.com>
2025-02-27 13:42:01 +0000

    Exercício 6, double infinity

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            double x = double. PositiveInfinity ;
+            double xi = double. PositiveInfinity ;
+            Console.WriteLine(x);
+            Console.WriteLine(xi);
+        }
+    }
+}
