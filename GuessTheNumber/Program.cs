cabee000609f1ff9c809ccf1a2d7d93452aa89bf
RicardoMota22 <ricardomota.artist@gmail.com>
2025-02-28 17:59:33 +0000

    Exercício de Avaliaçao, dome

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -12,35 +12,44 @@ namespace GuessTheNumber
             
             Console.WriteLine("Insert number: ");
             int aa = int.Parse(Console.ReadLine());
-
-            switch (response)
-            {
-                case (aa > numberToGuess);
-                    response = "The hidden number is lower than"{aa}".Try again.";
-                    break;
-                
-                case (aa < numberToGuess);
-                    response = "The hidden number is higher than"{aa}".Try again.";
-                    break;
-
-                case (aa < 0);
-                    response = "Numbers must be between 0 and 30.Try again.";
-                    break;
-
-                case (aa > 30);
-                    response = "Numbers must be between 0 and 30.Try again.";
-                    break;
-                
-                case (aa == numberToGuess);
-                    response = "You have found the hidden number"+ {numberToGuess}+ "after" + {} +"tries.";
-                    return;
-            }
-
             
 
-            
+            int response, tries; //Variavel auxilliar
+
+            tries = 0; 
+
+            //each loop = 1 tries 
+
+                switch (response)
+                {
+                    case (aa > numberToGuess);
+                        response = "The hidden number is lower than"{aa}".Try again.";
+                        tries = tries + 1;
+                        break;
+                    
+                    case (aa < numberToGuess);
+                        response = "The hidden number is higher than"{aa}".Try again.";
+                        tries = tries + 1;
+                        break;
+
+                    case (aa < 0);
+                        response = "Numbers must be between 0 and 30.Try again.";
+                        tries = tries + 1;
+                        break;
+
+                    case (aa > 30);
+                        response = "Numbers must be between 0 and 30.Try again.";
+                        tries = tries + 1;
+                        break;
+                    
+                    case (aa == numberToGuess);
+                        response = "You have found the hidden number"+ {numberToGuess}+ "after" + {tries} +"tries.";
+                        return;
+                }
 
             Console.WriteLine(numberToGuess);
+            Console.WriteLine(response);
+            while (response);
 
             
 
