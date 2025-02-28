e3e0170eb37cf07c8c65a61a7c3b2fce10517df6
RicardoMota22 <ricardomota.artist@gmail.com>
2025-02-28 17:11:36 +0000

    Exercício de Avaliaçao, pt 1

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int numberToGuess = rnd.Next(31);
+            
+            Console.WriteLine("Insert number: ");
+            int aa = int.Parse(Console.ReadLine());
+
+            switch(aa)
+            {
+                
+            }
+
+            Console.WriteLine(numberToGuess);
+
+            
+
+        }
+    }
+}
