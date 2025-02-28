d7101cd074eac266e506c74fc4a2c2b742d28539
RicardoMota22 <ricardomota.artist@gmail.com>
2025-02-28 16:19:57 +0000

    Exercício 10, EXIT works, other responses do not loop

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -6,40 +6,51 @@ namespace ChatGPTOne
     {
         private static void Main(string[] args)
         {
+            
+            
              // Variáveis auxiliares
-            string question, response;
+            //string question, response;
+
+            string question;
+            string response = "EXIT";
+            do
+            
         
-
-            // Colocar questão ao utilizador
-            Console.Write("Place your question? ");
-            question = Console.ReadLine();
-
-            // Verificar se se trata de uma questão conhecida e determinar uma
-            // resposta adequada
-            switch (question)
+        
             {
-                case "How are you?":
-                    response = "I'm fine, thank you";
-                    break;
-                case "What's your name?":
-                    response = "Gran'pa";
-                    break;
-                case "What's your mission?":
-                    response = "Destroy mankind!";
-                    break;
-                case "What's your favourite tea?":
-                    response = "coffee is better than tea!";
-                    break;
-                case "EXIT":
-                    return;
-                default:
-                    response = "I don't recognize that question";
-                    break;
-            }
+                // Colocar questão ao utilizador
+                Console.Write("Place your question? ");
+                question = Console.ReadLine();
 
+                // Verificar se se trata de uma questão conhecida e determinar uma
+                // resposta adequada
+                switch (question)
+                {
+                    case "How are you?":
+                        response = "I'm fine, thank you";
+                        break;
+                    case "What's your name?":
+                        response = "Gran'pa";
+                        break;
+                    case "What's your mission?":
+                        response = "Destroy mankind!";
+                        break;
+                    case "What's your favourite tea?":
+                        response = "coffee is better than tea!";
+                        break;
+                    case "EXIT": 
+                        return; //unlike break it does not affect the response
+                    default:
+                        response = "I don't recognize that question";
+                        break;
+                }
+            } while (response != "EXIT");
            
             // Responder adequadamente
             Console.WriteLine(response);
+            
+
+            
 
             
         }
